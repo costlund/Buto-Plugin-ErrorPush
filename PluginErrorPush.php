316c3e0273146a45e8793f1990bef86a2633cd72
Claes Östlund <>
2023-11-07 16:20:19 +0100

    1.1.2

--- PluginErrorPush.php
+++ PluginErrorPush.php
@@ -52,7 +52,7 @@ class PluginErrorPush{
       $post->set('error_message', $error->get('message'));
       $post->set('error_file', $error->get('file'));
       $post->set('error_line', $error->get('line'));
-      $post->set('error_type', $error->get('type'));
+      $post->set('error_type', PluginErrorPush::ErrorTypeToString($error->get('type')));
       foreach($server_variables as $v){
         $post->set($v, $server->get($v));
       }
@@ -61,5 +61,42 @@ class PluginErrorPush{
       $post->set('phpversion', phpversion());
       $this->push2($data->get('data/url'), $post->get());
     }
+  }
+  public static function ErrorTypeToString($type)
+  {
+    switch($type)
+      {
+      case E_ERROR: // 1 //
+          return 'E_ERROR';
+      case E_WARNING: // 2 //
+          return 'E_WARNING';
+      case E_PARSE: // 4 //
+          return 'E_PARSE';
+      case E_NOTICE: // 8 //
+          return 'E_NOTICE';
+      case E_CORE_ERROR: // 16 //
+          return 'E_CORE_ERROR';
+      case E_CORE_WARNING: // 32 //
+          return 'E_CORE_WARNING';
+      case E_CORE_ERROR: // 64 //
+          return 'E_COMPILE_ERROR';
+      case E_CORE_WARNING: // 128 //
+          return 'E_COMPILE_WARNING';
+      case E_USER_ERROR: // 256 //
+          return 'E_USER_ERROR';
+      case E_USER_WARNING: // 512 //
+          return 'E_USER_WARNING';
+      case E_USER_NOTICE: // 1024 //
+          return 'E_USER_NOTICE';
+      case E_STRICT: // 2048 //
+          return 'E_STRICT';
+      case E_RECOVERABLE_ERROR: // 4096 //
+          return 'E_RECOVERABLE_ERROR';
+      case E_DEPRECATED: // 8192 //
+          return 'E_DEPRECATED';
+      case E_USER_DEPRECATED: // 16384 //
+          return 'E_USER_DEPRECATED';
+      }
+    return $type;
   }
 }
